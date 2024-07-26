5a1b6a44df7b95539334d95a0e36c7dd96c8e0f9
frazah2 <tankionlinefraz@mail.com>
2024-07-26 17:57:34 +0400

    first commit

---
+++ js/i.js
@@ -0,0 +1,7 @@
+
+
+function menuClick(){
+    document.getElementsByClassName('MobileNavMenu')[0].classList.toggle('MobileNavMenuDeactivate')
+    document.getElementsByClassName('fa-times')[0].classList.toggle('theX')
+    document.getElementsByClassName('fa-bars')[0].classList.toggle('buuug')
+}
